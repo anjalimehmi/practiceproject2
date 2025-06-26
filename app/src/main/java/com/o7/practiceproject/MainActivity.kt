f2e070c09624453595ded7f9114585934d4619fe
anjalimehmi <anjalimehmi91@gmail.com>
2025-06-26 19:58:36 +0530

    Initial commit

---
+++ app/src/main/java/com/o7/practiceproject/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.o7.practiceproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var btn: Button?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btn=findViewById(R.id.scbtn1)
+        btn?.setOnClickListener {
+            Toast.makeText(this, "on click message displayed", Toast.LENGTH_SHORT).show()
+            var intent= Intent(this, edittextfetching::class.java)
+            startActivity(intent)
+        }
+    }
+}
